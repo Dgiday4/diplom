667ea3216390087e262e173157884f7dc8a0e44e
dgiday <nastya-ardasheva-84@mail.ru>
2025-12-08 21:20:42 +0500

    реализована страница с формой добавления собак

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dog
+from .views import dog, add_dog_with_form
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', dog)
+    path('', dog, name='main'),
+    path('dog/add_dog',add_dog_with_form, name='add_dog')
 ]
 
 if settings.DEBUG:
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Dog
+from .forms import DogModelForm
 
 
 def dog(request):
@@ -8,5 +9,20 @@ def dog(request):
     context = {'dog': dog}
     return render(request, 'main.html', context)
 
+def add_dog_with_form(request):
+    form = DogModelForm()
 
+    if request.method == 'POST':
+            print(request.POST)
+            print(request.FILES)
+            form = DogModelForm(request.POST, request.FILES)
+
+            if form.is_valid():
+                # создать объект в базе
+                dog = form.save()
+
+                return redirect('main')
+
+    context = {'form': form}
+    return render(request, 'add_dog.html', context)
 # Create your views here.
